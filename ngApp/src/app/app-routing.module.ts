cfc76cb33d8a324de942e908a92fd5921af3f4b5
eJanuel <januel.etienne@gmail.com>
2018-05-28 20:10:53 +0200

    first commit

---
+++ ngApp/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/register',
+    pathMatch: 'full'
+  },
+  {
+    path: 'chatbox',
+    component: ChatboxComponent
+  },
+  {
+    path: 'special',
+    component: SpecialEventsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
